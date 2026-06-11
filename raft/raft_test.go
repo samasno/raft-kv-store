cf8a9992dcb41ff731f60f62b5d07b6ea32b4c8e
samasno <samasno@samasno.com>
2026-06-11 10:01:49 -0500

    test with chan cycling, add test setup helper

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -3,54 +3,37 @@ package raft
 import "testing"
 
 func TestFollowerTicks(t *testing.T) {
-	id := uint64(9)
-	votedFor := uint64(7)
-	currentTerm := uint64(22)
-	lastLogIndex := uint64(77)
-	startTime := uint64(100)
-
-	startingEntry := RaftEntry{
-		Index: lastLogIndex,
-		Term:  currentTerm,
-	}
-
-	startingLog := []RaftEntry{startingEntry}
-
-	mlog := newInMemoryLogfile(startingLog)
-
-	conf := RaftConfig{id}
-
-	md := newInMemoryMetadataFile(votedFor, currentTerm)
-
-	r, err := NewRaftInstance(md, mlog, conf)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	r.time = startTime
+	r, defaults, _, _ := setupRaftTest()
 	r.electionTimeout = 4
 
 	for i := uint64(1); i <= 4; i++ {
 		r.Tick()
-		expectedTime := startTime + i
+		<-r.Ready()
+		expectedTime := defaults.time + i
 		assertEqual(t, "Time", r.time, expectedTime)
-		assertEqual(t, "State", raftStateString(r.currentState), raftStateString(raft_follower))
+		assertEqual(t, "State", r.currentState.String(), raft_follower.String())
 		assertEqual(t, "Election Elapsed", r.electionElapsed, i)
+		r.Advance()
 	}
 
 	r.Tick()
+	<-r.Ready()
+	r.Advance()
+	r.Done()
 
-	assertEqual(t, "State", raftStateString(r.currentState), raftStateString(raft_precandidate))
+	assertEqual(t, "State", r.currentState.String(), raft_precandidate.String())
 	assertEqual(t, "Election Elapsed", r.electionElapsed, 0)
-	assertEqual(t, "Voted For", r.votedFor, votedFor)
-	assertEqual(t, "Current Term", r.currentTerm, currentTerm)
-	assertEqual(t, "Last Log Index", r.lastEntryIndex, lastLogIndex)
+	assertEqual(t, "Voted For", r.votedFor, defaults.votedFor)
+	assertEqual(t, "Current Term", r.currentTerm, defaults.currentTerm)
+	assertEqual(t, "Last Log Index", r.lastEntryIndex, defaults.lastEntryIndex)
 	assert(t, r.electionTimeout > 9, "Election Timeout: Expected to be greater than 9")
 }
 
 func TestCallFollowerHeartBeatSameTerm(t *testing.T) {
 	// prep raft instance
-	// arbitrary ticks
+	// r, defaults, mdata, mlog := setupRaftTest()
+	// arbitrary ticks less than 10
+	// for
 	// call a heartbeat with same leader - should update leader
 	// update commit w/ output
 }
