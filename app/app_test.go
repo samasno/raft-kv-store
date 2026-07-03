d38594b0d1db098a1a06de382dc73f5998fa3cda
samasno <emmanuelwcamacho@gmail.com>
2026-07-03 13:16:54 -0700

    testing proposal

--- app/app_test.go
+++ app/app_test.go
@@ -5,5 +5,11 @@ import (
 )
 
 func TestControlLoop(t *testing.T) {
+	// set up 1 coordinator
+	// should start
+	// propose should return failure with 0 leader
+	// sm := newTestStateMachine(1)
+	// rpcConfig :=
+	// coordinator := NewCoordinator()
 
 }
--- app/coordinator.go
+++ app/coordinator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/samasno/raft-kv/raft"
@@ -26,9 +27,14 @@ type StateMachine interface {
 
 type RaftCoordinator struct {
 	// mutliplex channels for raft run loop
-	proposalc chan RaftProposal
+	proposalc chan RaftProposalRequest
 	messagec  chan raft.RaftMessage
 	donec     chan struct{}
+
+	mtx *sync.Mutex
+
+	commitIndex   uint64
+	commitIndexWC sync.Cond
 
 	// output handlers
 	logfile      *storage.LogFile
@@ -39,12 +45,26 @@ type RaftCoordinator struct {
 	stateMachine StateMachine
 }
 
-type RaftProposal struct {
-	Message raft.RaftMessage
-	Error   chan error
+func NewCoordinator() *RaftCoordinator {
+	return &RaftCoordinator{
+		proposalc:     make(chan RaftProposalRequest),
+		messagec:      make(chan raft.RaftMessage),
+		donec:         make(chan struct{}),
+		commitIndexWC: *sync.NewCond(&sync.Mutex{}),
+	}
 }
 
-func (rc *RaftCoordinator) StartControlLoop(raftConfig *raft.RaftConfig, rpcConfig *rpc.RaftServerConfig, storageDir string) error {
+type RaftProposalRequest struct {
+	Message  raft.RaftMessage
+	Response chan RaftProposalResponse
+}
+
+type RaftProposalResponse struct {
+	Success  bool
+	LeaderId uint64
+}
+
+func (rc *RaftCoordinator) StartControlLoop(raftConfig *raft.RaftConfig, rpcConfig *rpc.RaftServerConfig, storageDir string, stateMachine StateMachine) error {
 	var err error
 
 	rc.logfile, err = storage.OpenLogFile(storageDir)
@@ -64,6 +84,8 @@ func (rc *RaftCoordinator) StartControlLoop(raftConfig *raft.RaftConfig, rpcConf
 		return err
 	}
 	defer rc.rpc.Close()
+
+	rc.stateMachine = stateMachine
 
 	rc.raft, err = raft.NewRaftInstance(rc.metadatafile, rc.logfile, *raftConfig)
 	if err != nil {
@@ -89,7 +111,6 @@ func (rc *RaftCoordinator) StartControlLoop(raftConfig *raft.RaftConfig, rpcConf
 		}
 	}
 
-	// close log, meta, rpc
 	return nil
 }
 
@@ -120,6 +141,11 @@ func (rc *RaftCoordinator) handleMessage(msg raft.RaftMessage) {
 		println("todo err")
 	}
 
+	if 0 < len(output.ApplyEntries) {
+		rc.commitIndex = output.ApplyEntries[len(output.ApplyEntries)-1].Index
+		rc.commitIndexWC.Broadcast()
+	}
+
 	for _, msg := range output.SendMessages {
 		err = rc.rpc.SendMessage(msg)
 		if err != nil {
@@ -130,21 +156,59 @@ func (rc *RaftCoordinator) handleMessage(msg raft.RaftMessage) {
 	rc.raft.Advance()
 }
 
-func (rc *RaftCoordinator) handleProposal(pr RaftProposal) {
+func (rc *RaftCoordinator) handleProposal(preq RaftProposalRequest) {
+	presp := RaftProposalResponse{}
 
+	rc.raft.Call(preq.Message)
+	output := <-rc.raft.Ready()
+	if 0 < len(output.SendMessages) && output.SendMessages[0].Type == raft.MessageNotLeader {
+		msg := output.SendMessages[0]
+		presp.Success = false
+		presp.LeaderId = msg.LeaderId
+		preq.Response <- presp
+		return
+	}
+
+	// check if any write entries
+	var err error
+	err = rc.logfile.AppendEntries(output.WriteLogEntries)
+	if err != nil {
+		println("todo err")
+	}
+
+	for _, msg := range output.SendMessages {
+		err = rc.rpc.SendMessage(msg)
+		if err != nil {
+			println("todo err")
+		}
+	}
+
+	rc.raft.Advance()
+
+	lastEntry := output.WriteLogEntries[len(output.ApplyEntries)-1]
+
+	rc.commitIndexWC.L.Lock()
+	defer rc.commitIndexWC.L.Unlock()
+	for rc.commitIndex >= lastEntry.Index {
+		rc.commitIndexWC.Wait()
+	}
+
+	presp.Success = true
+	preq.Response <- presp
 }
 
 func (rc *RaftCoordinator) ProcessMessage(msg raft.RaftMessage) {
 	rc.messagec <- msg
 }
 
-func (rc *RaftCoordinator) Propose(msg raft.RaftMessage, resp chan error) {
-	proposal := RaftProposal{
-		Message: msg,
-		Error:   resp,
+func (rc *RaftCoordinator) Propose(msg raft.RaftMessage, resp chan RaftProposalResponse) {
+	proposal := RaftProposalRequest{
+		Message:  msg,
+		Response: resp,
 	}
 
 	rc.proposalc <- proposal
+	// if error returned, need to check leader and send there
 }
 
 func (rc *RaftCoordinator) Done() {
@@ -156,10 +220,72 @@ type testStateMachine struct {
 	values map[string]string
 }
 
+func newTestStateMachine(id uint64) *testStateMachine {
+	return &testStateMachine{
+		id:     id,
+		values: map[string]string{},
+	}
+}
+
 func (sm *testStateMachine) Apply(entries []raft.RaftEntry) error {
 	for _, e := range entries {
 		fmt.Printf("StateMachine #%d: index %d term %d", sm.id, e.Index, e.Term)
+		key := fmt.Sprintf("%d", e.Index)
+		sm.values[key] = string(e.Payload)
 	}
 
 	return nil
+}
+
+func (sm *testStateMachine) debugPrintValues() {
+	println("***Start StateMachine Values***")
+	for k, v := range sm.values {
+		fmt.Printf("%s: %s", k, v)
+	}
+	println("***End StateMachine Values***")
+}
+
+func testingRpcConfig(count int) map[uint64]*rpc.RaftServerConfig {
+	peers := map[uint64]rpc.Peer{}
+	// make peers
+	for i := uint64(1); i <= uint64(count); i++ {
+		peer := rpc.Peer{
+			Url: fmt.Sprintf("127.0.0.1:800%d", i),
+		}
+		peers[i] = peer
+	}
+
+	configs := map[uint64]*rpc.RaftServerConfig{}
+	for k, v := range peers {
+		config := &rpc.RaftServerConfig{
+			Id:    k,
+			Addr:  v.Url,
+			Peers: peers,
+		}
+
+		configs[k] = config
+	}
+	// make config for each peer
+	return configs
+}
+
+func testingRaftConfigs(count int) map[uint64]*raft.RaftConfig {
+	configs := map[uint64]*raft.RaftConfig{}
+	for i := uint64(1); i <= uint64(count); i++ {
+		peers := []uint64{}
+		for j := uint64(1); j <= uint64(count); j++ {
+			if j != i {
+				peers = append(peers, i)
+			}
+		}
+
+		config := &raft.RaftConfig{
+			Id:    i,
+			Peers: peers,
+		}
+
+		configs[i] = config
+	}
+
+	return configs
 }
